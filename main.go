79d7b3de40a846f29b82c0824811d1a02c7dcd56
kevinnp1093 <if-21009@students.ithb.ac.id>
2024-03-08 11:12:56 +0700

    main
    main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	http.Handle("/", router)
+	fmt.Println("Connected to port 8080")
+	log.Println("Connected to port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
